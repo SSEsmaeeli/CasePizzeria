ae2303a08d63223af32a274de83814616472f2e8
seyyed sajjad esmaeeli <s.sajjad.esmaeeli@gmail.com>
2022-07-13 19:21:27 +0430

    fix validation of order status update

--- src/src/ValidationRule/OrderValidStatusRule.php
+++ src/src/ValidationRule/OrderValidStatusRule.php
@@ -10,6 +10,22 @@ class OrderValidStatusRule extends OrderBaseRule
 
     public function execute(): bool
     {
-        return in_array($this->order->getStatus(), array_column(OrderStatus::cases(), 'value'));
+        return in_array(
+            $this->order->getStatus(),
+            $this->getAvailableActions()
+        );
+
+        // old method (Non-Sequential):
+        // use $this->getAllActions() instead of $this->getAvailableActions()
+    }
+
+    public function getAvailableActions(): array
+    {
+        return $this->order->getActions();
+    }
+
+    public function getAllActions(): array
+    {
+        return array_column(OrderStatus::cases(), 'value');
     }
 }
